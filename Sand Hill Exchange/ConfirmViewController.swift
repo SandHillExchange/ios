7757b66ff63e8b3371bda945b3ad9e2957aa1df5
elaineo <elaine.ou@gmail.com>
2015-05-13 20:47:00 -0700

    add confirmview

---
+++ Sand Hill Exchange/ConfirmViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ConfirmViewController.swift
+//  Sand Hill Exchange
+//
+//  Created by Elaine Ou on 5/13/15.
+//  Copyright (c) 2015 Sand Hill Exchange. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
